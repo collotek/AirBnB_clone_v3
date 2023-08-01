62a19dbd9b38d1defff90325cb403959ce3e654f
Enidbii <chebsenid@gmail.com>
2023-08-01 23:13:49 +0300

    changes

--- api/v1/app.py
+++ api/v1/app.py
@@ -2,12 +2,11 @@
 """ 
 Flask web application API.
 """
-import os
+from api.v1.views import app_views
 from flask import Flask, make_response, jsonify
 from flask_cors import CORS
-
 from models import storage
-from api.v1.views import app_views
+import os
 
 
 app = Flask(__name__)
